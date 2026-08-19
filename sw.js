c9989242554844b326b850ffc4cd917234a8c41d
gradyhorrocks-sudo <grady.horrocks@icloud.com>
2026-08-19 13:22:54 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mctr-current30-20260819';
+const CACHE='mctr-current33-20260819';
 const ASSETS=['./registry.json','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.skipWaiting();
